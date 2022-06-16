2c59a72b452a044584efda492b700bf5a9834e53
Victor Poblete <vpoblete@gmail.com>
2022-06-16 19:22:22 +0200

    Config and cards size

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,15 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git checkout -b main
+# First time
+# git init
+# git checkout -b main
+
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:vpoblete/vpoblete.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:vpoblete/masked-image.git main:gh-pages
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite';
-import path from "path";
 
 export default defineConfig({
   base: '/masked-image/',
-  resolve: {
-    alias: {
-      "~/": `${path.resolve(__dirname, "src")}/`,
-    },
-  },
 })
