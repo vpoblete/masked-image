c41c8a36502aafb93f2b0c65619113d06659554e
Victor Poblete <vpoblete@gmail.com>
2022-06-16 18:00:53 +0200

    Change for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import path from "path";
 
 export default defineConfig({
-  base: '/masked-image/'
+  base: '/masked-image/',
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
 })
